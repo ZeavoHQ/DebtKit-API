8dff8b9322182a23d87a98cffacdaaf1cb0092be
Zeavo <tom@zeavo.com>
2013-12-08 17:42:15 +0000

    Update lead.php
    The lead data is now passed as a JSON string under the `data` POST variable. Name, phone, email are mandatory fields. You can pass your own custom fields by adding further elements to the data array.

--- lead.php
+++ lead.php
@@ -2,27 +2,26 @@
 
 // Set the host
 $url    = 'https://api.debtkit.co.uk/';
-
+	
 // Set the POST parameters
 $fields = array(
-    'account'   =>  'YOUR ACCOUNT ID HERE',
-    'key'       =>  'YOUR API KEY HERE',
-    'name'      =>  urlencode( 'John Smith' ),
-    'phone'     =>  urlencode( '08001234567' ),
-    'email'     =>  urlencode( 'john.smith@gmail.com' ),
+    'account'   	=>  1,
+    'key'       	=>  '1RBrXUqIpUcyKma5',
+    'introducer'	=>	1,
+    'data'      	=>  json_encode( array( 'name' => $_POST[ 'name' ], 'phone' => $_POST[ 'phone' ], 'email' => $_POST[ 'email' ] ) )
 );
-
+	
 // Initialise the cURL request
 $ch = curl_init();
-
+	
 // Set the cURL options
 curl_setopt( $ch, CURLOPT_URL, $url );
 curl_setopt( $ch, CURLOPT_POST, count( $fields ) );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $fields ) );
-
+	
 // Execute the cURL request
 $result = curl_exec( $ch );
-
+	
 // Close the cURL request
 curl_close( $ch );
 
