3c6a1ca79a8eb98c7b1457f23cffd2454589448b
Zeavo <tom@zeavo.com>
2013-12-09 18:20:06 +0000

    Create get.php

---
+++ leads/get.php
@@ -0,0 +1,15 @@
+<?php
+
+$fields = array(
+    'account'           =>  999,
+    'key'               =>  '1RBrXUqIpUcyKma5'
+);
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, 'https://api.debtkit.co.uk/leads/get.json' );
+curl_setopt( $ch, CURLOPT_POST, count( $fields ) );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $fields ) );
+$result = curl_exec( $ch );
+curl_close( $ch );
+
+?>
