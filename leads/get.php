39959e1453c57d0028c11c2bb782edecfbe89e26
Zeavo <tom@zeavo.com>
2014-02-15 13:26:06 +0000

    Update get.php

--- leads/get.php
+++ leads/get.php
@@ -6,7 +6,7 @@ $fields = array(
 );
 
 $ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, 'https://api.debtkit.co.uk/leads/get.json' );
+curl_setopt( $ch, CURLOPT_URL, 'https://api.debtkit.co.uk/v1/leads/get.json' );
 curl_setopt( $ch, CURLOPT_POST, count( $fields ) );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $fields ) );
 $result = curl_exec( $ch );
