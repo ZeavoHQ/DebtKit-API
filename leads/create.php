6dbd6cfcd29f1551f872700f171c81eb3d73418c
Zeavo <tom@zeavo.com>
2014-02-15 13:25:59 +0000

    Update create.php

--- leads/create.php
+++ leads/create.php
@@ -9,7 +9,7 @@ $fields = array(
 );
 
 $ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, 'https://api.debtkit.co.uk/leads/create.json' );
+curl_setopt( $ch, CURLOPT_URL, 'https://api.debtkit.co.uk/v1/leads/create.json' );
 curl_setopt( $ch, CURLOPT_POST, count( $fields ) );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $fields ) );
 $result = curl_exec( $ch );
